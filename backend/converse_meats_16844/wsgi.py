9842d3d1c76ba8e884ffead0110092ba8eabbe92
Crowdbotics <team@crowdbotics.com>
2020-05-10 21:04:02 +0000

    Update models

--- backend/converse_meats_16844/wsgi.py
+++ backend/converse_meats_16844/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'converse_meats_16844.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "converse_meats_16844.settings")
 
 application = get_wsgi_application()
